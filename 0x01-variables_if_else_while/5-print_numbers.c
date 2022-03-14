efcb7234a9f49ee232dface6f8bce22a1d3b37b8
shirleylimo <shirleyjlimo@gmail.com>
2022-03-14 14:26:42 -0700

    print single digit nums

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: 0
+ */
+int main(void)
+{
+	int no;
+
+	for (no = 0; no < 10; no++)
+	{
+		printf("%d", no);
+	}
+	printf("\n");
+	return (0);
+}
