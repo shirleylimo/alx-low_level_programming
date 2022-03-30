f690626ebc7335010fe991e7f13ba2e4944b079d
shirleylimo <shirleyjlimo@gmail.com>
2022-03-30 10:02:11 -0700

    compare two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two strings using shell expansion
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: 1 if strings are identical, 0 otherwise.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
