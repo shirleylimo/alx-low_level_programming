c1e4d536761f619677f65739870bb62e93c8968e
shirleylimo <shirleyjlimo@gmail.com>
2022-03-30 09:32:03 -0700

    print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print reverse
+ * @s: input string
+ *
+ * Description: prints a string in reverse
+ * Return: always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
