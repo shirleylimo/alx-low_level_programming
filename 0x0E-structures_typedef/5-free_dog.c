e0941d9891865f35f967b49cc6960b4ceb0e4728
shirleylimo <shirleyjlimo@gmail.com>
2022-04-11 12:19:24 -0700

    structures

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ *
+ * @d: dog to free
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
