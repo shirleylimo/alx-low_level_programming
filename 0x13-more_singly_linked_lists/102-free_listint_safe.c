c7881e2f317a98ad685514adbefd51e0cb7d7e7d
shirleylimo <shirleyjlimo@gmail.com>
2022-04-26 13:38:09 -0700

    frees list 2

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,39 +1,63 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a listint_t list
- * @h: double pointer to head of list
- * Return: number of nodes freed
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
  */
-size_t free_listint_safe(listint_t **h)
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
-	listint_t *current;
-	listint_t *next;
-	int diff;
+	listint_t **newlist;
+	size_t i;
 
-	register short count = 0;
-
-	if (!h || !(*h))
-		return (count);
-	current = *h;
-	while (current)
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
 	{
-		diff = current - current->next;
-		if (diff > 0)
-		{
-			next = current->next;
-			free(current);
-			current = next;
-			count++;
-		} else
-		{
-			free(current);
-			*h = NULL;
-			count++;
-			break;
-		}
-
+		free(list);
+		exit(98);
 	}
-	*h = NULL;
-	return (count);
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * free_listint_safe - frees a listint_t linked list.
+ * @head: double pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	size_t i, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
+
+	if (head == NULL || *head == NULL)
+		return (num);
+	while (*head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (*head == list[i])
+			{
+				*head = NULL;
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (num);
 }
